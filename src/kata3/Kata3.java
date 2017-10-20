0738880cb773c944582050cf9575d23ac1af7260
usuario <usuario@10.22.148.68>
2017-10-20 09:29:53 +0100

    "trabajamos con histromas"

---
+++ src/kata3/Kata3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata3;
+
+/**
+ *
+ * @author usuario
+ */
+public class Kata3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        HistogramDisplay histo=new HistogramDisplay();
+        histo.execute();
+    }
+}
